7e8fc856a9468b76f22a7d4e80fc0429a4cb07c8
Alif829 <bsse1120@iit.du.ac.bd>
2023-02-07 15:37:22 +0600

    Rating field added

--- Movie-Server/Migrations/20230207091917_RatingCol.Designer.cs
+++ Movie-Server/Migrations/20230207091917_RatingCol.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Movie_Server.Data;
 
@@ -11,9 +12,11 @@ using Movie_Server.Data;
 namespace MovieServer.Migrations
 {
     [DbContext(typeof(MovieDataContext))]
-    partial class MovieDataContextModelSnapshot : ModelSnapshot
+    [Migration("20230207091917_RatingCol")]
+    partial class RatingCol
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Movie-Server/Program.cs
+++ Movie-Server/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.WebHost.UseStaticWebAssets();
 builder.Services.AddDbContext<MovieDataContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
