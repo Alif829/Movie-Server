39d64fe24eddbcb4888796c4fba8176e9c521a1e
Alif829 <bsse1120@iit.du.ac.bd>
2023-02-07 12:18:52 +0600

    CRUD operations added

---
+++ Movie-Server/Data/MovieDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movie_Server.Models;
+
+namespace Movie_Server.Data
+{
+	public class MovieDataContext : DbContext
+	{
+		public MovieDataContext(DbContextOptions<MovieDataContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Movie> Movie { get; set; }
+	}
+}
